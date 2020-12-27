da6c3349311b8453a78eb239b72554eab2aace89
maxchistt <maks.novohatsckij@yandex.ru>
2020-12-28 01:26:22 +0300

    changeGitName

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
     <header>
         <h1>Шляпа 👒</h1>
-        <span><a style="transition: 0.1s;" href="https://github.com/maksnovohatsckij/shlyapa" target="_blank" class="text-secondary">GitHub</a></span>
+        <span><a style="transition: 0.1s;" href="https://github.com/maxchistt/shlyapa" target="_blank" class="text-secondary">GitHub</a></span>
     </header>
 
     <div id="menu">
